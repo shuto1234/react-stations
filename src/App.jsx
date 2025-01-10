40aa6c4f18db7ff7c1b4441b6234b782a9a80339
shuto1234 <171531334+shuto1234@users.noreply.github.com>
2025-01-10 17:14:47 +0900

    station5クリア

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,25 @@
 // DO NOT DELETE
 
 import './App.css'
+import React,{useState} from 'react'
 
 /**
  * @type {() => JSX.Element}
  */
 export const App = () => {
+
+  const [dogUrl, setDocUrl] = useState("https://images.dog.ceo/breeds/hound-plott/hhh-23456.jpg")
+
   return (
-    <div>
-      <h2>Hello, world!</h2>
-    </div>
+    <>
+      <header>Dogアプリ</header>
+      <body>
+        <p>犬の画像を表示するサイトです</p>
+        <img src={dogUrl}></img>
+        <p>
+          <button onClick={() => setDocUrl("https://images.dog.ceo/breeds/hound-english/n02089973_1132.jpg")}>クリック</button>
+        </p>
+      </body>
+    </>
   )
 }
