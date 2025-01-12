775e84d48b891f5050b05f42a08fcca24ab03896
shuto1234 <171531334+shuto1234@users.noreply.github.com>
2025-01-12 19:19:40 +0900

    station6クリア

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,48 @@
 // DO NOT DELETE
 
 import './App.css'
-import React,{useState} from 'react'
+import React,{useEffect, useState} from 'react'
 
 /**
  * @type {() => JSX.Element}
  */
 export const App = () => {
 
+  
+  // fetch("https://dog.ceo/api/breeds/image/random")
+  //   .then(response => {
+    //     console.log("データを受け取った。", response)
+    //   })
+  // fetch("https://dog.ceo/api/breeds/image/random")
+  //   .then((res) => res.json())
+  //   .then((json) => {
+  //     console.log("データを受け取った", json);
+  //   })
+
   const [dogUrl, setDocUrl] = useState("https://images.dog.ceo/breeds/hound-plott/hhh-23456.jpg")
+  
+  function loadedDog () {
+    // useEffect(() => {
+      fetch("https://dog.ceo/api/breeds/image/random")
+        .then(response => {
+              console.log("データを受け取った。", response)
+              return response
+            })
+        .then(res => res.json())
+        .then(response => {
+          console.log("JSONに変換されたデータを受け取った", response);
+          return response
+        })
+        .then((result) => 
+            // setDocUrl(result)
+            {
+            setDocUrl(result.message);
+          },
+          (error) => {
+            setDocUrl(error);
+          }); 
+      // },[]);
+  }
 
   return (
     <>
@@ -17,8 +51,11 @@ export const App = () => {
         <p>犬の画像を表示するサイトです</p>
         <img src={dogUrl}></img>
         <p>
-          <button onClick={() => setDocUrl("https://images.dog.ceo/breeds/hound-english/n02089973_1132.jpg")}>クリック</button>
+          <button onClick={loadedDog}>クリック</button>
         </p>
+        <div>
+          {dogUrl}
+        </div>
       </body>
     </>
   )
