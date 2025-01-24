f72e2164807464834218734217f59c71550d5a23
shuto1234 <171531334+shuto1234@users.noreply.github.com>
2025-01-24 12:18:18 +0900

    station11クリア

--- src/BreedsSelect.jsx
+++ src/BreedsSelect.jsx
@@ -2,35 +2,45 @@
 import { useState } from "react";
 // @ts-ignore
 
-export const BreedsSelect = ({ breeds, selectBreed, setSelectBreed, onChange }) => {
+export const BreedsSelect = ({ breeds, selectedBreed, onChange, dogList, clickChange }) => {
 
-  const [ selectedBreed, setSelectedBreed ] = useState()
+
   console.log(breeds);
 
 
   const dogs = Object.keys(breeds);
-  // const selectDogs = dogs.map((dog) =>
-  //   <li key={dog.toString()}>{dogs}</li>
-  // );
   console.log("dogs", dogs)
+
+  const imageDogs = Object.values(dogList)
+  console.log("imageDogs", imageDogs)
   
+
   return (
     <>   
       <div>
-        <select 
-          value={selectedBreed} 
-          onChange={onChange
-            // (event) => console.log("選択したデータ", event.target.value)
-            // setSelectBreed(value)
-          }
-        >
-          {dogs.map((dog) => (
-            <option key={dog} value={dog}>
-              {dog}
-            </option>
-            ))}
-        </select>
-            <p>{selectBreed}わからない</p>
+        <h3 className="h3">選択してください</h3>
+        <div className="flex">
+          <select 
+            value={selectedBreed} 
+            onChange={onChange
+              // (event) => console.log("選択したデータ", event.target.value)
+              // setSelectBreed(value)
+            }
+          >
+              <option>select</option>
+            {dogs.map((dog) => (
+              <option key={dog} value={dog}>
+                {dog}
+              </option>
+              ))}
+          </select>
+          <button className="button2" onClick={clickChange} >表示</button>
+        </div>
+      </div>
+      <div className="imageDogs">
+        {imageDogs.map((imageDog) => (
+          <img src={imageDog} className="imageDog"/>
+        ))}
       </div>
     </>
   );
--- src/DogListContainer.jsx
+++ src/DogListContainer.jsx
@@ -3,13 +3,52 @@ import { useEffect, useState } from "react";
 import BreedsSelect from "./BreedsSelect";
 
 export const DogListContainer = () => {
-  const [breeds, setBreeds] = useState("https://dog.ceo/api/breeds/list/all")
-  // const [ selectBreed, setSelectBreed ] = useState()
+  const [breeds, setBreeds] = useState("")
   const [ selectedBreed, setSelectedBreed ] = useState()
-  const changeBreed = (event) => {
-    setSelectedBreed(event.target.value);
+  const [ dogList, setDogList ] = useState("")
+  function onChange(e) {
+    setSelectedBreed(e.target.value);
     console.log("selectedBreedの確認", selectedBreed)
-  } 
+    fetch('selectedBreed')
+    // .then(res1 => {
+    //   console.log("selectedBreed", res1)
+    //   return res1
+    // })
+    // console.log("dogBreedUrlの確認", dogBreedUrl)
+  }
+  
+  function clickChange () {
+    const dogBreedUrl = `https://dog.ceo/api/breed/${selectedBreed}/images/random/12`;
+    // const dogBreedUrl = `https://dog.ceo/api/breed/hound/images/random/3`;
+    // setDogList(dogBreedUrl.toString())
+    fetch(dogBreedUrl)
+      .then(res1 => {
+        console.log("dogBreedUrlデータを受け取った", res1)
+        return res1
+      })
+      .then(res2 => res2.json())
+      .then(res3 => {
+        console.log("dogBreedUrlのJSONのデータを取得", res3)
+        return res3
+      })
+      .then((res4) => 
+        {setDogList(res4.message)}
+      );
+    
+    // const dogImage = new URL(`https://dog.ceo/api/breed/${selectedBreed}/images/random`)
+      // fetch(dogImage)
+      // .then(res1 => {
+      //   console.log("データを受け取った", res1)
+      //   return res1
+      // })
+      // .then(res2 => res2.json())
+      // .then(res3 => {
+      //   console.log("JSONのデータを取得", res3)
+      //   return res3
+      // })
+      // .then((res4) => dogImage = res4);
+  }
+  
   useEffect(() => {
     fetch("https://dog.ceo/api/breeds/list/all")
       .then(res1 => {
@@ -26,10 +65,13 @@ export const DogListContainer = () => {
       );
   },[]);
 
+
   console.log("breedsの中身の確認", breeds)
 
   return (
-      <BreedsSelect breeds={breeds} selectBreed={selectedBreed} setSelectBreed={undefined} onChange={changeBreed} />
+    <>
+      <BreedsSelect breeds={breeds} selectedBreed={selectedBreed} onChange={onChange} dogList={dogList} clickChange={clickChange} />
+    </>
   );
 }
 
